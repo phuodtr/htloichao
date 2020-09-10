daffa0e50b4bec03cb196f8c4ccf341ed73a75b0
phuodtr <phuoczsmm@gmail.com>
2020-09-10 11:44:01 +0700

    htloichao
    htloichao

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace htloichao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter your name:  ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello : " + name);
+            Console.ReadKey();
+
+        }
+    }
+}
