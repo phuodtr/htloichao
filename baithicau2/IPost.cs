fa20e8c097c11454f4b6345e58931b65e27b0e61
phuodtr <phuoczsmm@gmail.com>
2020-10-02 09:56:55 +0700

    phuodtr

---
+++ baithicau2/IPost.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baithicau2
+{
+    public interface IPost
+    {
+        public void Display();
+        public void CalculatorRate();
+    }
+}
